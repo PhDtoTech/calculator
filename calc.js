fab6293258ef56cf058b33fec1a944ccab212db5
Paulina Ramirez <paulina.rn@gmail.com>
2022-12-12 22:04:41 -0600

    Fix display function to display multiple numbers

--- calc.js
+++ calc.js
@@ -19,7 +19,48 @@ const multiply = function(a,b) {
 };
 
 
-function display () {
+
+
+function display() {
+    const entry = document.getElementById("entry");
+
+    const numbers = document.querySelectorAll(".num");
+
+    const operators = document.querySelectorAll(".op");
+
+    numbers.forEach(number => {
+        number.addEventListener('click', () => {
+            console.log (number.textContent);
+            entry.textContent += number.textContent;
+        });
+    });
+
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+            console.log (operator.textContent);
+            entry.textContent = operator.textContent;
+        });
+    });
+
+}
+
+function clear () {
+    const clear = document.getElementById("clear");
+
+    clear.addEventListener('click', () => {
+        entry.textContent = null;
+    });
+}
+
+
+
+// let firstNumber = 
+
+
+
+
+
+/* function display () {
     const numberButtons = document.querySelectorAll('.num');
   
     numberButtons.forEach(numberButton => {
@@ -32,4 +73,14 @@ function display () {
     const displayValue = numberButtons.textContent;
 
     return displayValue;
-  }
+  } */
+
+  /* const buttons = document.querySelectorAll(".button");
+
+    buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      console.log (button.textContent);
+      document.getElementById("entry").textContent= button.textContent;
+    });
+  }); */
+
