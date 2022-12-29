4a626646df2c820459083e885cae99064c0189d5
Paulina Ramirez <paulina.rn@gmail.com>
2022-12-28 22:24:26 -0600

    Fix operate functionality

--- calc.js
+++ calc.js
@@ -4,7 +4,7 @@ let numA = "";
 
 let numB = "";
 
-function operate(add) {
+function operate() {
     return (add);
 };
 
@@ -24,84 +24,62 @@ const multiply = function(numA,numB) {
     return (numA * numB);
 };
 
+const previousOperand = document.querySelector(".previous-operand");
+const currentOperand = document.querySelector(".current-operand");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".op");
+const equalButton = document.getElementById("equal");
+const clearButton = document.getElementById("clear");
 
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
+        if (previousOperand.textContent == "=" || previousOperand.textContent == "÷" || previousOperand.textContent == "x" || previousOperand.textContent == "-" || previousOperand.textContent == "+") {
+            previousOperand.textContent = "";
+        } 
+        
+        previousOperand.textContent += number.textContent;
 
-document.addEventListener('readystatechange', function() {
-    if (document.readyState === "complete") {
-      display();
-      clear ();
-      saveNums ();
+        if (op=="") {
+            numA += number.textContent;
+            console.log("numA is " + numA);
+            return numA;
+        }
+        else {
+            numB += number.textContent;
+            console.log("numB is " + numB);
+            return numB;
+        }
+
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', () => {
+        previousOperand.textContent += " " + operator.textContent + " ";
+        op=operator.textContent;
+        console.log(op + " " + typeof op);
+    });
+});
+    
+equalButton.addEventListener('click', ()=> {
+    previousOperand.textContent = "=";
+    if (numA!="" && numB!="" && op!="") {
+        numA=numA*1;
+        numB=numB*1;
+        console.log(typeof numA + op + typeof numB );
+        if (op=="÷") {currentOperand.textContent = divide(numA,numB)}
+        else if (op=="-") {currentOperand.textContent = subtract(numA,numB)}
+        else if (op=="+" ) {currentOperand.textContent = add(numA,numB)}
+        else if (op=="x") {currentOperand.textContent = multiply(numA,numB)};
     }
-  });
+})
 
-
-function display() {
-    
-    const entry = document.getElementById("entry");
-    const numbers = document.querySelectorAll(".num");
-    const operators = document.querySelectorAll(".op");
-    const equalButton = document.getElementById("equal");
-
-    numbers.forEach(number => {
-        number.addEventListener('click', () => {
-            if (entry.textContent == "=" || entry.textContent == "÷" || entry.textContent == "x" || entry.textContent == "-" || entry.textContent == "+") {
-                entry.textContent = "";
-            } 
-            entry.textContent += number.textContent;
-
-        });
-    });
-
-    operators.forEach(operator => {
-        operator.addEventListener('click', () => {
-            entry.textContent = operator.textContent;
-            op=operator.textContent;
-            console.log(op + " " + typeof op);
-        });
-    });
-    
-    equalButton.addEventListener('click', ()=> {
-        entry.textContent = "=";
-    })
-
-}
-
+clearButton.addEventListener('click', clear);
 
 function clear () {
-    const entry = document.getElementById("entry");
-    const clearButton = document.getElementById("clear");
-    clearButton.addEventListener('click', clear);
-    entry.textContent = "";
-}
-
-function saveNums () {
-    const numbers = document.querySelectorAll(".num");
-    const equalButton = document.getElementById("equal");
-
-    numbers.forEach(number => {
-        number.addEventListener('click', () => {
-            if (op=="") {
-                numA += number.textContent;
-                console.log("numA is " + numA);
-                return numA;
-            }
-            else {
-                numB += number.textContent;
-                console.log("numB is " + numB);
-                return numB;
-            }
-        })}
-    )
-
-    equalButton.addEventListener('click', () =>{
-        if (numA!="" && numB!="" && op!="") {
-            numA=numA*1;
-            numB=numB*1;
-            console.log(numA + op + numB );
-            if (op=="÷") {return operate(divide)}
-            else if (op=="-") {return operate(subtract)}
-            else if (op=="+" ) {return operate(add)}
-            else if (op=="x") {return operate(multiply)};
-        }
-    })
+    previousOperand.textContent = "";
+    currentOperand.textContent = "";
+    op = "";
+    numA = "";
+    numB = "";
 }
